a118c511651477c84a2e07f028cad77c2566be36
Jesse Pretorius (odyssey4me) <jesse@odyssey4.me>
2020-03-28 16:38:45 +0000

    [tests] Use `ansible.module_utils.urls.open_url` instead of `urlopen`

--- tests/utils/shippable/check_matrix.py
+++ tests/utils/shippable/check_matrix.py
@@ -10,17 +10,12 @@ import re
 import sys
 import time
 
+from ansible.module_utils.urls import open_url
+
 try:
     from typing import NoReturn
 except ImportError:
     NoReturn = None
-
-try:
-    # noinspection PyCompatibility
-    from urllib2 import urlopen  # pylint: disable=ansible-bad-import-from
-except ImportError:
-    # noinspection PyCompatibility
-    from urllib.request import urlopen
 
 
 def main():  # type: () -> None
@@ -47,7 +42,7 @@ def main():  # type: () -> None
 
     for attempts_remaining in range(4, -1, -1):
         try:
-            jobs = json.loads(urlopen('https://api.shippable.com/jobs?runIds=%s' % run_id).read())
+            jobs = json.loads(open_url('https://api.shippable.com/jobs?runIds=%s' % run_id).read())
 
             if not isinstance(jobs, list):
                 raise Exception('Shippable run %s data is not a list.' % run_id)
