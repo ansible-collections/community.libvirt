c6beecdfb35b50e8bc79200b51449c4584e73a16
Jens Heinrich <github.com/JensHeinrich>
2021-09-14 08:20:51 +0200

    Add doc_fragment for requirements

---
+++ plugins/doc_fragments/requirements.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+class ModuleDocFragment(object):
+    DOCUMENTATION = r"""
+requirements:
+    - python >= 2.6
+    - libvirt python bindings
+    """
