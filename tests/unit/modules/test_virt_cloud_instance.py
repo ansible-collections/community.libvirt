f2487d91975c5a47957c853d6886c23b7b0b5cf1
Joey Zhang <thinkdoggie@gmail.com>
2025-09-19 07:10:39 +0800

    refactor: Extract common libvirt functionality to module_utils (#216)
    - Move LibvirtConnection and other reusable constants to plugins/module_utils/libvirt.py
    - Add comprehensive unit tests for the LibvirtConnection class

--- tests/unit/modules/test_virt_cloud_instance.py
+++ tests/unit/modules/test_virt_cloud_instance.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+#
+# (c) 2025, Joey Zhang <thinkdoggie@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 import unittest
 
